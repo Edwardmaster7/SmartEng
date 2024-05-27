9887a3ae358c4a50f7555c630de64688c719c31d
Edwardmaster7 <eduardoobatista2002@hotmail.com>
2024-05-27 13:06:04 -0300

    Implementação básica da tabela orçamento com TanstackTable

---
+++ smartengApp/src/components/Table/BasicTable.jsx
@@ -0,0 +1,90 @@
+import { useReactTable, getCoreRowModel, flexRender } from "@tanstack/react-table";
+import mdata from "./data.json";
+import { useMemo } from "react";
+import { Container } from "../Container";
+
+export function BasicTable() {
+    
+    const data = useMemo(() => mdata, [])
+
+    /**@type {import("@tanstack/react-table").ColumnDef<any>} */
+    const columns = [
+      {
+        header: "Item",
+        accessorKey: "Item",
+      },
+      {
+        header: "Base",
+        accessorKey: "Base",
+      },
+      {
+        header: "Código",
+        accessorKey: "Código",
+      },
+      {
+        header: "Descrição",
+        accessorKey: "Descrição",
+      },
+      {
+        header: "Unidade",
+        accessorKey: "Unidade",
+      },
+      {
+        header: "Qtd.",
+        accessorKey: "Qtd",
+      },
+      {
+        header: "VU - Material",
+        accessorKey: "VU-Material",
+      },
+      {
+        header: "VU - M.O",
+        accessorKey: "VU-M.O",
+      },
+      {
+        header: "Total",
+        accessorKey: "Total",
+      },
+    ];
+
+    const table = useReactTable( {
+        data, columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+    return (
+      <Container className="col-span-4 row-span-8 mx-auto gap-1 overflow-auto p-6">
+        <table className="table-auto border border-slate-400 ">
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id} className="border border-slate-300">
+                    {header.isPlaceholder ? null : (
+                      <div>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                      </div>
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id} className="border border-slate-300">
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Container>
+    );
+}
--- smartengApp/src/pages/Quote.jsx
+++ smartengApp/src/pages/Quote.jsx
@@ -4,38 +4,84 @@ import { InputField } from "../components/InputField";
 import { ButtonComponent } from "../components/ButtonComponent";
 import { Container } from "../components/Container";
 import { Field } from "../components/Field";
-
+import { BasicTable } from "../components/Table/BasicTable";
 
 function Quote() {
     return (
-        <div className="w-full h-screen">
-            <Header />
-            <Main className="p-3 grid-rows-4 align-center gap-3">      
-            
-                <Container className="bg-indigo pt-0 pb-2 mx-auto row-span-1 col-span-4 rounded contain-content overflow-auto">
-                    <div className="bg-indigo-600 rounded-t-lg flex justify-center p-2 mb-2"><span className="text-indigo-50 font-medium">Orçamento da Obra - Analítico</span></div>
+      <div className="h-screen w-full">
+        <Header />
+        <Main className="align-center grid-rows-4 gap-3 p-3">
+          <Container className="bg-indigo col-span-4 row-span-1 mx-auto overflow-auto rounded pb-2 pt-0 contain-content">
+            <div className="mb-2 flex justify-center rounded-t-lg bg-indigo-600 p-2">
+              <span className="font-medium text-indigo-50">
+                Orçamento da Obra - Analítico
+              </span>
+            </div>
 
-                    <div className="grid sm:grid-cols-4 px-2 gap-1 place-content-evenly max-auto grid-cols-2">
-                        <Field fieldName="Cliente:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">John Doe</Field>
-                        <Field fieldName="Telefone:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">19 996958543</Field>
-                        <Field fieldName="Email:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">email@exemplo.com</Field>
-                        <Field fieldName="Data Revisão:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">22/05/2024</Field>
-                        
-                        <Field fieldName="Obra:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">John Doe</Field>
-                        <Field fieldName="Data de validade:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">John Doe</Field>
-                        <Field fieldName="BDI:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">37%</Field>
-                        
-                        <Field fieldName="Encargos Sociais:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">John Doe</Field>
-                        <Field fieldName="Endereço:" className="min-h-1 sm:text-md mt-0 sm:gap-1 bg-white rounded-md p-1 shadow-md text-sm gap-0 min-w-min lg:flex-row lg:gap-1">Av. Paulista, n 8958 Estados unidios</Field>
-                    </div>
-                </Container>
-                    
-                <Container className="p-6 mx-auto col-span-4 gap-1 row-span-8">
-                         
-                </Container>
-            </Main>
-        </div>
-    )
+            <div className="max-auto grid grid-cols-2 place-content-evenly gap-1 px-2 sm:grid-cols-4">
+              <Field
+                fieldName="Cliente:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                John Doe
+              </Field>
+              <Field
+                fieldName="Telefone:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                19 996958543
+              </Field>
+              <Field
+                fieldName="Email:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                email@exemplo.com
+              </Field>
+              <Field
+                fieldName="Data Revisão:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                22/05/2024
+              </Field>
+
+              <Field
+                fieldName="Obra:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                John Doe
+              </Field>
+              <Field
+                fieldName="Data de validade:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                John Doe
+              </Field>
+              <Field
+                fieldName="BDI:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                37%
+              </Field>
+
+              <Field
+                fieldName="Encargos Sociais:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                John Doe
+              </Field>
+              <Field
+                fieldName="Endereço:"
+                className="sm:text-md mt-0 min-h-1 min-w-min gap-0 rounded-md bg-white p-1 text-sm shadow-md sm:gap-1 lg:flex-row lg:gap-1"
+              >
+                Av. Paulista, n 8958 Estados unidios
+              </Field>
+            </div>
+          </Container>
+
+          <BasicTable></BasicTable>
+        </Main>
+      </div>
+    );
 }
 
 export default Quote;
