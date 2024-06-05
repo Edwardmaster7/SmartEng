b3972bef3aece6fe94b3a0fa6ed4b4587fcb9520
Edwardmaster7 <eduardoobatista2002@hotmail.com>
2024-06-05 12:06:15 -0300

    Ajustes na página Cliente

--- smartengApp/src/components/Modal.jsx
+++ smartengApp/src/components/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = ({ children, isOpen, onClose, className }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
+    <div className="fixed inset-0 flex items-center justify-center z-50 min-w-80">
       <div className={`absolute bg-violet-600 p-6 rounded-xl shadow-2xl ${className}`}>
         <HiXCircle
           className="text-2xl text-violet-100 absolute right-2 top-2 hover:text-red-500 hover:animate-pulse"
